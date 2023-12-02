7e74a075d3a7cb892f686fdc82681b4c7de64160
Lucas-F913 <89488384+Lucas-F913@users.noreply.github.com>
2023-12-02 14:15:53 -0500

    population size Label+entry+ created function.py for use later

--- project.py
+++ project.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import functions
 #import os
 
 
@@ -22,6 +23,15 @@ def submit_generation():
 ## For now, prediction first
 def make_chart(gender, generation):
     pass
+
+def get_gender():
+    name = gender_entry.get()
+    return name
+
+def get_generation():
+    name = generation_entry.get()
+    return name
+
 
 
 ## Main function for main_test.py
@@ -52,6 +62,11 @@ generation_label = tk.Label(root, text="What generation (1-4)", font=("Roboto", 
 generation_label.place(x=125, y=275)
 generation_entry = tk.Entry(root)
 generation_entry.place(x=125, y=300)
+
+population_size = tk.Label(root, text="What is the population size?", font=("Roboto", 12, 'bold'))
+population_size.place(x=400, y=275)
+population_entry = tk.Entry(root)
+population_entry.place(x=450,y=300)
 
 generation_submit_button = tk.Button(root, text="Submit", command=submit_generation)
 generation_submit_button.place(x=165, y=325)
